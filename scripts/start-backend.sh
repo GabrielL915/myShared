e5a2bd7d240d883cf5fab2158a0a998939e5d9e1
GabrielL915 <gabrielluizgomes1@gmail.com>
2024-08-19 15:55:31 -0300

    add frontend and backend

---
+++ scripts/start-backend.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ../app/BackEnd
+
+# Executa o Spring Boot usando Maven (ou Gradle, se aplicável)
+./mvnw spring-boot:run
---
+++ scripts/start-frontend.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ../app/FrontEnd
+
+# Instala as dependências usando pnpm (ou npm/yarn, se aplicável)
+pnpm install
+
+pnpm run dev
+
+# Para npm, use:
+# npm install
+# npm run dev
+
+# Para yarn, use:
+# yarn install
+# yarn dev
