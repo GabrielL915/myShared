c17d7a50ec14ea773a57d53d2ceda14ac82ab176
GabrielL915 <gabrielluizgomes1@gmail.com>
2024-08-23 18:26:45 -0300

    change response to a record

--- app/backend/src/main/java/com/myShared/backend/domain/dto/LoginResponseDTO.java
+++ app/backend/src/main/java/com/myShared/backend/domain/dto/LoginResponseDTO.java
@@ -1,14 +1,9 @@
 package com.myShared.backend.domain.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
+
 @Builder
-@AllArgsConstructor
-public class LoginResponseDTO {
-    private final String token;
-    private final Long expiresIn;
+public record LoginResponseDTO(String token, Long expiresIn) {
 }
 
