5cb6bdc098a534fddd274510fa856b93d274f72d
GabrielL915 <gabrielluizgomes1@gmail.com>
2024-09-10 17:17:32 -0300

    add main layout

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
+import FileHistory from "./components/feature/FileHistory";
 import { Button } from "./components/ui/button";
+import MainLayout from "./layout/MainLayout";
 
 const mockData = [
   {
@@ -30,36 +32,10 @@ const mockData = [
 
 function App() {
   return (
-    <div className="text-customText">
-      <aside className="fixed left-0 top-0 h-full w-64 bg-custombackground p-4 border-r-2 border-white">
-        <h2 className="text-lg font-bold mb-4 text-center">File History</h2>
-        <ul>
-          {mockData.map((item) => (
-            <li
-              key={item.id}
-              className="flex items-center mb-3 p-2 bg-customcard rounded hover:bg-gray-800"
-            >
-              <img
-                src={item.icon}
-                alt={`${item.name} icon`}
-                className="w-8 h-8 mr-3"
-              />
-              <div>
-                <p className="text-sm font-medium">{item.name}</p>
-                <p className="text-xs text-gray-400">{item.type}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </aside>
-
-      <div className="ml-64 p-6">
-        <header className="App-header">
-          <p>MyShared</p>
-        </header>
-        <Button>Send File</Button>
-      </div>
-    </div>
+    <MainLayout>
+      <FileHistory files={mockData} />
+      <Button>Click me</Button>
+    </MainLayout>
   );
 }
 
