9c91a4a66fb33e4bf93bf645ea0b139a4b948578
GabrielL915 <gabrielluizgomes1@gmail.com>
2024-09-13 10:18:07 -0300

    create send file input

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,6 +1,8 @@
+import React from "react";
 import "./App.css";
 import FileHistory from "./components/feature/FileHistory";
 import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 import MainLayout from "./layout/MainLayout";
 
 const mockData = [
@@ -31,10 +33,43 @@ const mockData = [
 ];
 
 function App() {
+  const [fileInfo, setFileInfo] = React.useState<string | null>(null);
+  const fileInputRef = React.useRef<HTMLInputElement | null>(null);
+
+  const handleFileClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      setFileInfo(files[0].name);
+    }
+  };
+
   return (
     <MainLayout>
       <FileHistory files={mockData} />
-      <Button>Click me</Button>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div className="flex items-center space-x-1">
+          <Input placeholder="Enter id" />
+          <Button onClick={handleFileClick}>Send File</Button>
+          <input
+            type="file"
+            ref={fileInputRef}
+            onChange={handleFileChange}
+            style={{ display: 'none' }}
+          />
+        </div>
+
+        {fileInfo && (
+          <div className="mt-2 w-full text-center text-customText">
+            <pre>{fileInfo}</pre>
+          </div>
+        )}
+      </div>
     </MainLayout>
   );
 }
